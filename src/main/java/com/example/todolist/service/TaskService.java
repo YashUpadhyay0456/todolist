5d87bbee615a756d1309cf4163e8672842ec6c32
Yash Upadhyay <yashuchanu@gmail.com>
2025-05-20 09:08:32 +0530

    needed to commit to do work on different device

--- src/main/java/com/example/todolist/service/TaskService.java
+++ src/main/java/com/example/todolist/service/TaskService.java
@@ -1,9 +1,11 @@
 package com.example.todolist.service;
 
+import java.util.List;
+
 import com.example.todolist.entities.Tasks;
 
 public interface TaskService {
 
     public Tasks createTask(Tasks tasks);
-    
+    List<Tasks> geTasksByUserId(Long userId);
 }
--- src/main/java/com/example/todolist/serviceImpl/TaskServiveImpl.java
+++ src/main/java/com/example/todolist/serviceImpl/TaskServiveImpl.java
@@ -1,5 +1,7 @@
 package com.example.todolist.serviceImpl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +18,10 @@ public class TaskServiveImpl implements TaskService {
     @Override
     public Tasks createTask(Tasks task) {
         return taskRepository.save(task);
+    }
+    @Override
+    public List<Tasks> geTasksByUserId(Long userId){
+        return taskRepository.findById(userId);
     }
     
 }
